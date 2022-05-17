d94d8e91a01f35bc57da8a6e7b5573c5462a5f8c
vinicius.pereira <viniibp@gmail.com>
2022-05-17 18:30:43 -0300

    ajuste footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,10 @@
 export const Footer = () => {
   return (
-    <footer className="absolute right-0 left-0 bottom-0 h-[300px] bg-red-700 mx-auto">
-        <h1>eae</h1>
+    <footer className="absolute right-0 left-0 bottom-0 lg:h-[300px] bg-red-700 mx-auto">
+        <div className="lg:w-[1024px] mx-auto">
+          <h1>eae</h1>
+          <h1>eae</h1>
+        </div>
     </footer>
   );
 }
